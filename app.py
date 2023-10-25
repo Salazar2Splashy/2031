984ee745c3a28f7c8897a76557935ba85a8fc889
Salazar2Splashy <c2012921@ncl.ac.uk>
2023-10-25 17:34:10 +0100

    Completed all of Task 2 - Error Handling.

--- app.py
+++ app.py
@@ -33,5 +33,32 @@ app.register_blueprint(users_blueprint)
 app.register_blueprint(admin_blueprint)
 app.register_blueprint(lottery_blueprint)
 
+
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template('errors/500.html'), 500
+
+
+@app.errorhandler(400)
+def internal_error(error):
+    return render_template('errors/400.html'), 400
+
+
+@app.errorhandler(403)
+def internal_error(error):
+    return render_template('errors/403.html'), 403
+
+
+@app.errorhandler(503)
+def internal_error(error):
+    return render_template('errors/503.html'), 503
+
+
+@app.errorhandler(404)
+def internal_error(error):
+    return render_template('errors/404.html'), 404
+
+
 if __name__ == "__main__":
     app.run()
+
